099997bc5c5b79282e066c788eb4b7e772927228
Husam <almarranihusam@gmail.com>
2018-05-01 14:18:01 +0100

    optimized css hover for home button. finishing part 1 of team section

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ $(window).on('load', function(){
     $('#preloader').delay(350).fadeOut();
 });
 
-
 $(function(){
-    $(".owl-carousel").owlCarousel();
-})
+    /*===========Owl carousel======*/
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        autoplay: true,
+        loop: true,
+        autoplayHoverPause: true,
+        smartSpeed: 500,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });   
+    
+});
